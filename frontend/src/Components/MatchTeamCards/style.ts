17f03d08f82116cd327b15fc073cb9a7242a9359
Diego Moronha <diegomoronha@gmail.com>
2023-04-16 20:35:43 -0300

    delete components folder

--- frontend/src/Components/MatchTeamCards/style.ts
+++
@@ -1,9 +0,0 @@
-import { Theme } from '@mui/material/styles';
-import { makeStyles } from 'tss-react/mui';
-
-export const useStyles = makeStyles()((theme: Theme) => ({
-  cardsGrid: {
-    display: 'flex',
-    flexDirection: 'row',
-  },
-}));
