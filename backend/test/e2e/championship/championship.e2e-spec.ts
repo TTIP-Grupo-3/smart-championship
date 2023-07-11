a0673e93084cb65d3f0077dd2040cafdc35e7192
Adrián Cardozo <cardozoadrian997@gmail.com>
2023-07-10 23:52:47 -0300

    Fix all tests

--- backend/test/e2e/championship/championship.e2e-spec.ts
+++ backend/test/e2e/championship/championship.e2e-spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from 'src/app.module';
-import { params, championship, SECRET } from '../../data/e2e/championship/championship.e2e-spec.data.json';
+import { params, championship } from '../../data/e2e/championship/championship.e2e-spec.data.json';
 import { StorageService } from 'src/services/storage.service';
 import { mock } from 'test/utils/tests';
 
@@ -10,7 +10,6 @@ describe('ChampionshipController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
-    process.env.JWT_SECRET = SECRET;
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     })
